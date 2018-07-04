4666d89e29b553c28e73ad17817b9e51f7f96899
Jose Blandon <jose.blandon@ceiba.com.co>
2018-07-04 16:27:43 -0500

    Cambios en App8

--- App8Forms/src/app/components/template/template.component.ts
+++ App8Forms/src/app/components/template/template.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styleUrls: ['./template.component.ts']
 })
 export class TemplateComponent implements OnInit {
+
+  usuario: Object = {
+    nombre: 'Antonio',
+    apellido: 'blandon',
+    correo: 'mail@correo.com'
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  guardar(forma: NgForm) {
+    console.log('formulario posteado');
+    console.log(forma);
+    console.log(forma.value);
+    console.log(this.usuario);
   }
 
 }
