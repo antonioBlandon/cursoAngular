ef3425cb3a50fcbdb2f7add4f7855bfdc2a102ab
Jose Blandon <jose.blandon@ceiba.com.co>
2018-07-09 13:00:28 -0500

    hasta la clase 94

--- App4HttpSpotiApp/src/app/services/spotify.service.ts
+++ App4HttpSpotiApp/src/app/services/spotify.service.ts
@@ -10,10 +10,22 @@ export class SpotifyService {
 
   getNewReleases() {
   
+    //El token solo es valido por una hora. Si el servicio falla, esto deberia ser lo primero que se revise
     const headers = new HttpHeaders({
-      'Authorization':'Bearer BQDMM_oDamTngYzzJlOjAVANCjZgDeL8VUeRP6kmSGta_9zy-_nYQLC69RPwsY-XuQC6CcQLZYxMDvlb-sg'
+      'Authorization':'Bearer BQDy4_AJ7a8t6ufiVbNhHff1ONYLSco1NmVATCizCq3ZH_M_WtXLcpmN5OBkp1a-aX4TdYikggIJrNMJkQ8'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
   }
+
+  getArtist(termino: string) {
+  
+    //El token solo es valido por una hora. Si el servicio falla, esto deberia ser lo primero que se revise
+    const headers = new HttpHeaders({
+      'Authorization':'Bearer BQDy4_AJ7a8t6ufiVbNhHff1ONYLSco1NmVATCizCq3ZH_M_WtXLcpmN5OBkp1a-aX4TdYikggIJrNMJkQ8'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
+  }
+
 }
