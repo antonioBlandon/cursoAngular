7005f58de889596dd134164f2b39a42ee5907ab3
Jose Blandon <jose.blandon@ceiba.com.co>
2018-07-09 10:43:06 -0500

    Se creo servicio para consumir el servicio de los paises que hablan español

--- App4HttpSpotiApp/src/app/app.module.ts
+++ App4HttpSpotiApp/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// Importar Rutas
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -16,6 +21,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
+    HttpClientModule,
+    RouterModule.forRoot( ROUTES, { useHash: true} ),
     BrowserModule
   ],
   providers: [],
--- App4HttpSpotiApp/src/app/components/home/home.component.ts
+++ App4HttpSpotiApp/src/app/components/home/home.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PaisesService } from '../../services/paises.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  paises: any[] = [];
 
-  constructor() { }
+  constructor(private paisesService: PaisesService) { 
+    this.paisesService.getAllCountries().subscribe( (result: any[]) => {
+      this.paises = result;
+      console.log(this.paises);
+      
+    });
+  }
 
   ngOnInit() {
   }
